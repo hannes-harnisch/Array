70460d3c79bec5eca61dcedc1996fc20d819b1e9
Hannes Harnisch <hannes.harnisch@gmx.net>
2021-08-05 00:22:59 +0200

    Initial commit

---
+++ premake5.lua
@@ -0,0 +1,27 @@
+workspace 'StaticList'
+	architecture		'x64'
+	configurations		{ 'Debug', 'Release' }
+	flags				{ 'MultiProcessorCompile' }
+	startproject		'Tests'
+
+outputdir = '%{cfg.buildcfg}_%{cfg.architecture}_%{cfg.system}'
+
+project 'Tests'
+	location			'Tests'
+	kind				'ConsoleApp'
+	language			'C++'
+	cppdialect			'C++latest'
+	staticruntime		'On'
+	warnings			'Extra'
+	objdir				('.bin_int/' .. outputdir .. '/%{prj.name}')
+	targetdir			('.bin/'	 .. outputdir .. '/%{prj.name}')
+	files				{ '**.cpp', '**.hpp' }
+	includedirs			''
+
+	filter 'configurations:Debug'
+		runtime			'Debug'
+		symbols			'On'
+
+	filter 'configurations:Release'
+		runtime			'Release'
+		optimize		'Speed'
